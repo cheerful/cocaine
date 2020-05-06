a52c5c4ff861d151f23072cf0bc541f1907871de
Thomas Fuchs <thomas@slash7.com>
2020-05-06 11:35:48 -0700

    silence deprecation warning

--- lib/cocaine.rb
+++ lib/cocaine.rb
@@ -3,10 +3,12 @@
 require 'terrapin'
 Cocaine = Terrapin
 
-$stderr.puts [
-  "===========================================\n",
-  "DEPRECATION: The cocaine gem is deprecated.",
-  "Please upgrade to terrapin.",
-  "See https://github.com/thoughtbot/terrapin/ for further instructions.",
-  "\n==========================================="
-].join(" ")
+# silence warning
+#
+# $stderr.puts [
+#  "===========================================\n",
+#  "DEPRECATION: The cocaine gem is deprecated.",
+#  "Please upgrade to terrapin.",
+#  "See https://github.com/thoughtbot/terrapin/ for further instructions.",
+#  "\n==========================================="
+#].join(" ")
